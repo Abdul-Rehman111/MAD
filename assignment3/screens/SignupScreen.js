eda84ebe13b68e41f72bd4ed98a3ce8a8e20b722
Abdul-Rehman111 <100258023+Abdul-Rehman111@users.noreply.github.com>
2023-05-27 17:21:35 +0500

    Update SignupScreen.js

--- assignment3/screens/SignupScreen.js
+++ assignment3/screens/SignupScreen.js
@@ -68,7 +68,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     padding: 16,
-    backgroundColor:'blue'
+    backgroundColor:'orange'
   },
   title: {
     fontSize: 24,
